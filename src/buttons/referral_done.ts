ae86b445d290af246babc8e9c498b56a86d67ed4
Webhead <webheadvr@gmail.com>
2022-01-02 14:37:13 -0800

    Change from deleted to archived
    Archives threads instead of deleting

--- src/buttons/referral_done.ts
+++ src/buttons/referral_done.ts
@@ -3,17 +3,17 @@ import { Button } from "../classes/button";
 import { ButtonContext } from "../classes/buttonContext";
 
 
-export default class Test extends Button{
-    constructor(){
+export default class Test extends Button {
+    constructor() {
         super()
         this.name = "referral_done"
         this.regex = /referral_done/
         this.staffOnly = false
     }
     async run(ctx: ButtonContext): Promise<any> {
-        if(!ctx.interaction.channel?.isThread()) return ctx.error("You can only use this button in threads")
+        if (!ctx.interaction.channel?.isThread()) return ctx.error("You can only use this button in threads")
         ctx.deferUpdate({})
         let channel = ctx.interaction.channel
-        channel.delete().catch(() => channel.setArchived(true))
+        channel.setArchived(true)
     }
 }
