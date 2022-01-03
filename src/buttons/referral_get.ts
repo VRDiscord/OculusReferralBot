c128519aa3d6d23b297b804685a8558fc374284d
Zelda_Fan <78901316+ZeldaFan0225@users.noreply.github.com>
2022-01-03 11:15:28 +0100

    added better and more efficiently randomized db calls

--- src/buttons/referral_get.ts
+++ src/buttons/referral_get.ts
@@ -27,13 +27,16 @@ export default class Test extends Button {
     }
     async run(ctx: ButtonContext): Promise<any> {
         let region = ctx.customId.split("_")[1] ?? "non-us"
-        let data = await ctx.sql.query(`SELECT * FROM referrals WHERE region='${region}' ORDER BY RANDOM() LIMIT 1`).catch(() => null)
+        let data = await ctx.sql.query(`SELECT * FROM referrals TABLESAMPLE BERNOULLI (100) WHERE region='${region}' ORDER BY RANDOM()`).catch(() => null)
+        let row = data?.rows.sort(() => Math.random() > 0.5 ? -1 : 1)?.[0]
+        if(!row) return ctx.error("No referral URL has been found for your region")
 
-        if (!data) return ctx.error("No referral has been found for your region")
         if (ctx.interaction.channel?.type === "DM") return null
         if (ctx.interaction.channel?.isThread()) return ctx.error("You can't use this button in Threads")
 
         let channel = (ctx.interaction.channel as TextChannel)
+        let threads = await channel.threads.fetchActive().catch(() => null)
+        if(threads?.threads.find(t => t.name.includes(ctx.interaction.member?.user.username ?? ""))) return ctx.error(`You already have a thread open (<#${threads?.threads.find(t => t.name.includes(ctx.interaction.member?.user.username ?? ""))!.id}>).\nGo there or close it first to open a new one.`, {codeblock: false})
 
         let thread = await channel.threads.create({
             name: `Referral - ${region} - ${ctx.interaction.member?.user.username}`,
@@ -48,18 +51,18 @@ export default class Test extends Button {
         let embed = new MessageEmbed()
             .setColor("AQUA")
             .setTitle(`Your referral URL for ${region.toUpperCase()}`)
-            .setDescription(`**Submitter** <@${data.rows[0].discord_id}> (\`${data.rows[0].discord_id}\`)\n**URL** ${decodeURI(data.rows[0].url)}\n\nIf the link didn't work, please try tagging the user <@${data.rows[0].discord_id}> \n\nIf you spoke with the user, or rather try another link please click the red button.`)
+            .setDescription(`**Submitter** <@${row.discord_id}> (\`${row.discord_id}\`)\n**URL** ${decodeURI(row.url)}\n\nIf the link didn't work, please try tagging the user <@${row.discord_id}> \n\nIf you spoke with the user, or rather try another link please click the red button.`)
 
         ctx.reply({ content: `The referral is waiting for you in <#${thread.id}>`, ephemeral: true })
         await thread.members.add(ctx.interaction.member?.user.id!)
 
         thread.send({
-            content: decodeURI(data.rows[0].url),
+            content: decodeURI(row.url),
             embeds: [embed],
             components: components(region)
         })
 
         ctx.log(`${ctx.member.user.tag} (\`${ctx.interaction.member?.user.id}\`) got the referral url <${data!.rows[0].url}> submitted by ${data!.rows[0].discord_id}`)
-        await ctx.sql.query(`UPDATE referrals SET uses='${Number(data.rows[0].uses ?? 0) + 1}' WHERE discord_id='${data.rows[0].discord_id}'`).catch(() => null)
+        await ctx.sql.query(`UPDATE referrals SET uses='${Number(row.uses ?? 0) + 1}' WHERE discord_id='${row.discord_id}'`).catch(() => null)
     }
 }
