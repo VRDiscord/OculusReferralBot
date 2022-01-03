b3a723895050eeab32560bcae55e22e01a96999f
Webhead <webheadvr@gmail.com>
2022-01-03 08:33:45 -0800

    Update createpanel.ts
    Update embed and labels

--- src/commands/createpanel.ts
+++ src/commands/createpanel.ts
@@ -15,12 +15,12 @@ let components = [{
         type: 2,
         customId: "referral_us",
         style: 1,
-        label: "US"
+        label: "I need a US Referral"
     }, {
         type: 2,
         customId: "referral_non-us",
         style: 1,
-        label: "Non-US"
+        label: "I need a Non-US Referral"
     }]
 }]
 
@@ -38,10 +38,9 @@ export default class Test extends Command {
             .setTitle("Quest Referrals")
             .setDescription(`You can either add your own referral to be selected from, or request a referral below.`)
             .addFields(
-                { name: 'I just got my Quest and want to be referred', value: 'Select your region below, a new thread will be created with your referral.\n', inline: false },
-                { name: 'I have had my Quest for a while and want to be referred', value: 'If you recently (under 48 hours ago) activated your device without a referral, you may attempt a factory reset. \n You will need to re-download any games or apps and your game data that does not support [cloud saves](https://support.oculus.com/articles/in-vr-experiences/oculus-features/cloud-sync/) will be lost. \nIf you wish to attempt a factory reset, follow [this guide](https://support.oculus.com/articles/fix-a-problem/troubleshoot-headsets-and-accessories/troubleshooting-factory-reset-quest-2/), and before setting it back up grab a referral link by clicking your region below. \n', inline: false },
-                { name: 'I want to refer someone', value: 'Please type /submitreferral below, and select your region and submit your link. \n If you have issues please let us know. \n Referrals are automatically handed out at random when new users request them.', inline: false },
-                { name: '\u200B', value: '\u200B' },
+                { name: '__I just got my Quest and want to be referred__', value: 'Select your region below, a new thread will be created with your referral.\n', inline: false },
+                { name: '__I have had my Quest for a while and want to be referred__', value: 'If you recently (under 48 hours ago) activated your device without a referral, you may attempt a factory reset. \n You will need to re-download any games or apps and your game data that does not support [cloud saves](https://support.oculus.com/articles/in-vr-experiences/oculus-features/cloud-sync/) will be lost. \nIf you wish to attempt a factory reset, follow [this guide](https://support.oculus.com/articles/fix-a-problem/troubleshoot-headsets-and-accessories/troubleshooting-factory-reset-quest-2/), and before setting it back up grab a referral link by clicking your region below. \n', inline: false },
+                { name: '__I want to refer someone__', value: 'Please type /submitreferral below, and select your region and submit your link. \n If you have issues please let us know. \n Referrals are automatically handed out at random when new users request them.', inline: false },
             )
 
         ctx.reply({ content: "Done", ephemeral: true })
