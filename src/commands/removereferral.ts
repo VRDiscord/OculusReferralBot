ccaf503eb51240d1008bd51f73bf1fbdfb5aac09
Webhead <webheadvr@gmail.com>
2022-01-03 08:34:10 -0800

    Update removereferral.ts
    changed "uses" to  "Seen"

--- src/commands/removereferral.ts
+++ src/commands/removereferral.ts
@@ -9,8 +9,8 @@ const commandData: ApplicationCommandData = {
     description: "Removes your submitted referral url"
 }
 
-export default class Test extends Command{
-    constructor(){
+export default class Test extends Command {
+    constructor() {
         super(commandData)
         this.name = commandData.name
         this.staffOnly = false
@@ -18,20 +18,20 @@ export default class Test extends Command{
     async run(ctx: CommandContext): Promise<any> {
         let id = ctx.member.id
         let data = await ctx.sql.query(`SELECT * FROM referrals WHERE discord_id='${id}'`).catch(() => null)
-        if(!data || !data?.rows?.length) return ctx.error("Unable to find any set up referral links for you")
+        if (!data || !data?.rows?.length) return ctx.error("Unable to find any set up referral links for you")
 
         await ctx.sql.query(`DELETE FROM referrals WHERE discord_id='${id}'`).catch(() => null)
-        
+
         let log = new MessageEmbed()
-        .setTitle(`Referral removed`)
-        .setColor("#FEE75C")
-        .setDescription(`**User** ${ctx.member.user.tag} (\`${ctx.interaction.member?.user.id}\`)`)
-        .addFields([
-            {name: `**URL**`, value: decodeURI(data.rows[0].url)},
-            {name: `**Region**`, value: data.rows[0].region, inline: true},
-            {name: `**Uses**`, value: data.rows[0].uses, inline: true},
-        ])
+            .setTitle(`Referral removed`)
+            .setColor("#FEE75C")
+            .setDescription(`**User** ${ctx.member.user.tag} (\`${ctx.interaction.member?.user.id}\`)`)
+            .addFields([
+                { name: `**URL**`, value: decodeURI(data.rows[0].url) },
+                { name: `**Region**`, value: data.rows[0].region, inline: true },
+                { name: `**Uses**`, value: data.rows[0].uses, inline: true },
+            ])
         ctx.log(log)
-        ctx.reply({content: `Your referral url ${decodeURI(data.rows[0].url)} for **${data.rows[0].region}** with **${data.rows[0].uses}** uses has been deleted.`, ephemeral: true})
+        ctx.reply({ content: `Your referral url ${decodeURI(data.rows[0].url)} for **${data.rows[0].region}** with **${data.rows[0].uses}** times being seen has been deleted.`, ephemeral: true })
     }
 }
