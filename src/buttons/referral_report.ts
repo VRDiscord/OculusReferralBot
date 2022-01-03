3e82874845d6643f9e7242411cb1c8b1a8793285
Zelda_Fan <78901316+ZeldaFan0225@users.noreply.github.com>
2022-01-03 11:20:44 +0100

    added more efficient randomized db calls

--- src/buttons/referral_report.ts
+++ src/buttons/referral_report.ts
@@ -26,25 +26,29 @@ export default class Test extends Button {
     }
     async run(ctx: ButtonContext): Promise<any> {
         let region = ctx.customId.split("_")[2] ?? "non-us"
-        let data = await ctx.sql.query(`SELECT * FROM referrals WHERE region='${region}' ORDER BY RANDOM() LIMIT 1`).catch(() => null)
-        if (!data) return ctx.error("No other referral has been found for your region")
+        let reported = ctx.interaction.message.content
+        let data = await ctx.sql.query(`SELECT * FROM referrals TABLESAMPLE BERNOULLI (100) WHERE region='${region}' AND NOT url=$1 ORDER BY RANDOM()`, [reported]).catch(() => null)
+        let row = data?.rows.sort(() => Math.random() > 0.5 ? -1 : 1)?.[0]
+        if (!row) return ctx.error("No other referral has been found for your region")
         if (ctx.interaction.channel?.type === "DM") return null
         if (!ctx.interaction.channel?.isThread()) return ctx.error("You can't use this button in Threads")
 
         let embed = new MessageEmbed()
             .setColor("AQUA")
             .setTitle(`Your new referral URL for ${region.toUpperCase()}`)
-            .setDescription(`**Submitter** <@${data.rows[0].discord_id}> (\`${data.rows[0].discord_id}\`)\n**URL** ${decodeURI(data.rows[0].url)}\n\nIf the link didn't work press the red button\n\nIf you're done or selected the wrong region click the green button.`)
-
+            
+        
+        if(region === "us") embed.setDescription(`**Submitter** <@${row.discord_id}> (\`${row.discord_id}\`)\n**URL** ${decodeURI(row.url)}\n\nIf the link didn't work press the red button\n\nIf you're done or selected the wrong region click the green button.`)
+        else embed.setDescription("non-us")
+        
         ctx.reply({
-            content: decodeURI(data.rows[0].url),
+            content: decodeURI(row.url),
             embeds: [embed],
             components: components(region)
         })
 
-        let reported = ctx.interaction.message.content
         ctx.log(`${ctx.member.user.tag} (\`${ctx.interaction.member?.user.id}\`) reported the referral URL <${reported}> for **${region}**`)
 
-        await ctx.sql.query(`UPDATE referrals SET uses='${Number(data.rows[0].uses ?? 0) + 1}' WHERE discord_id='${data.rows[0].discord_id}'`).catch(() => null)
+        await ctx.sql.query(`UPDATE referrals SET uses='${Number(row.uses ?? 0) + 1}' WHERE discord_id='${row.discord_id}'`).catch(() => null)
     }
 }
